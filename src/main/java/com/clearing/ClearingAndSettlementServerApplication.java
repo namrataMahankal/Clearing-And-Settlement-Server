106203051b5e74b7a3ef1a613db01034f7b847eb
namrataMahankal <namrata1997@gmail.com>
2020-09-22 00:56:19 +0530

    initial commit

---
+++ src/main/java/com/clearing/ClearingAndSettlementServerApplication.java
@@ -0,0 +1,13 @@
+package com.clearing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearingAndSettlementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearingAndSettlementServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clearing/ClearingAndSettlementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clearing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearingAndSettlementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
