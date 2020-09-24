4f20eab752e347171a2861206e1801b5b0c6a117
lolcoder <chetan.arora02@gmail.com>
2020-09-24 11:01:32 +0530

    Added other packages in Spring Application

--- src/main/java/com/clearing/ClearingAndSettlementServerApplication.java
+++ src/main/java/com/clearing/ClearingAndSettlementServerApplication.java
@@ -2,7 +2,13 @@ package com.clearing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan("entity")
+@EnableJpaRepositories("repository")
+@ComponentScan("controller")
 @SpringBootApplication
 public class ClearingAndSettlementServerApplication {
 
