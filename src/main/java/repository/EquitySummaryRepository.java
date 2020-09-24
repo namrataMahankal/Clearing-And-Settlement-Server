0e7518fc26a52c55a4e62490ad2fc26f7e197e9f
Aotoge <kadambari.anuranjana@gmail.com>
2020-09-24 17:09:25 +0530

    Fix Interface parameters in EquitySummaryRepository

--- src/main/java/repository/EquitySummaryRepository.java
+++ src/main/java/repository/EquitySummaryRepository.java
@@ -2,11 +2,12 @@ package repository;
 import org.springframework.data.repository.CrudRepository;
 
 import entity.EquitySummary;
+import entity.EquitySummaryId;
 
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface EquitySummaryRepository extends CrudRepository<EquitySummary, Integer> {
+public interface EquitySummaryRepository extends CrudRepository<EquitySummary, EquitySummaryId> {
     
 }
