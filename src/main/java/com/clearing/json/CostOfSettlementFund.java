789ac81274540c37098cec11ed15ee7c9467ef90
eunicepaulson <49234462+eunicepaulson@users.noreply.github.com>
2020-09-29 15:46:52 +0530

    Added COst of Settlement in json

---
+++ src/main/java/com/clearing/json/CostOfSettlementFund.java
@@ -0,0 +1,41 @@
+package com.clearing.json;
+
+public class CostOfSettlementFund {
+
+	private float totalCost;
+	private int fundsToBeBorrowed;
+	private float borrowingRate;
+	private float costIncurred;
+
+	public CostOfSettlementFund(float totalCost, int fundsToBeBorrowed, float borrowingRate, float costIncurred) {
+		super();
+		this.fundsToBeBorrowed = fundsToBeBorrowed;
+		this.borrowingRate = borrowingRate;
+		this.costIncurred = costIncurred;
+	}
+
+	public int getFundsToBeBorrowed() {
+		return fundsToBeBorrowed;
+	}
+
+	public void setFundsToBeBorrowed(int fundsToBeBorrowed) {
+		this.fundsToBeBorrowed = fundsToBeBorrowed;
+	}
+
+	public float getBorrowingRate() {
+		return borrowingRate;
+	}
+
+	public void setBorrowingRate(float borrowingRate) {
+		this.borrowingRate = borrowingRate;
+	}
+
+	public float getCostIncurred() {
+		return costIncurred;
+	}
+
+	public void setCostIncurred(float costIncurred) {
+		this.costIncurred = costIncurred;
+	}
+
+}
