299129f0d4f196797603e86c1f09fcf5b0ef226c
eunicepaulson <49234462+eunicepaulson@users.noreply.github.com>
2020-09-30 10:22:14 +0530

    Create trades market price

--- src/main/java/com/clearing/json/CorporateActionList.java
+++ src/main/java/com/clearing/json/CorporateActionList.java
@@ -3,86 +3,57 @@ package com.clearing.json;
 import java.util.Map;
 
 public class CorporateActionList {
-	
+
 	private String securityName;
 	private int initialShareBalance;
 	private int finalShareBalance;
 	private String action;
 	private String parameter;
-	
-	
-	public CorporateActionList(String securityName, int initialShareBalance, int finalShareBalance,
-			String action, String parameter) {
+
+	public CorporateActionList(String securityName, int initialShareBalance, int finalShareBalance, String action,
+			String parameter) {
 		this.securityName = securityName;
 		this.initialShareBalance = initialShareBalance;
 		this.finalShareBalance = finalShareBalance;
 		this.action = action;
 		this.parameter = parameter;
 	}
-	
-	
+
 	public String getSecurityName() {
 		return securityName;
 	}
-
-
-
 
 	public void setSecurityName(String securityName) {
 		this.securityName = securityName;
 	}
 
-
-
-
 	public int getInitialShareBalance() {
 		return initialShareBalance;
 	}
-
-
-
 
 	public void setInitialShareBalance(int initialShareBalance) {
 		this.initialShareBalance = initialShareBalance;
 	}
 
-
-
-
 	public int getFinalShareBalance() {
 		return finalShareBalance;
 	}
-
-
-
 
 	public void setFinalShareBalance(int finalShareBalance) {
 		this.finalShareBalance = finalShareBalance;
 	}
 
-
-
-
 	public String getAction() {
 		return action;
 	}
-
-
-
 
 	public void setAction(String action) {
 		this.action = action;
 	}
 
-
-
-
 	public String getParameter() {
 		return parameter;
 	}
-
-
-
 
 	public void setParameter(String parameter) {
 		this.parameter = parameter;
